ecfa12f42659e94d60302fc6d7a28aff61e3bcf0
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-12-18 01:09:19 -0500

    Add public-samples/brian-16dec-zpcir1/src/hello_world/hello_world.py

---
+++ src/hello_world/hello_world.py
@@ -0,0 +1,7 @@
+def main() -> str:
+    """Returns a simple hello world greeting.
+
+    Returns:
+        str: Returns the string 'Hello, World!' as an immutable string literal
+    """
+    return 'Hello, World!'
